4bf660424eced7a9953d018ececbd68fd5d81ac5
dog9307 <35515667+dog9307@users.noreply.github.com>
2025-08-19 09:22:48 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 1.15 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int left, int right) {
+    int answer = 0;
+    
+    for (int i = left; i <= right; ++i)
+    {
+        int count = 1;
+        for (int j = 1; j < i; ++j)
+        {
+            if (i % j == 0)
+            {
+                ++count;
+            }
+        }
+        
+        answer += ((count % 2 == 0 ? i : -i));
+    }
+    
+    return answer;
+}
