2c6f5b96ce2436192ca18a524d538b9f1127f1b6
dog9307 <35515667+dog9307@users.noreply.github.com>
2025-08-22 09:45:06 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.52 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+    vector<int> answer;
+    
+    for (int i = 0; i < numbers.size() - 1; ++i)
+    {
+        for (int j = i + 1; j < numbers.size(); ++j)
+        {
+            int sum = numbers[i] + numbers[j];
+            
+            bool isAdd = true;
+            for (int k = 0; k < answer.size(); ++k)
+            {
+                if (answer[k] == sum)
+                {
+                    isAdd = false;
+                    break;
+                }
+            }
+            
+            if (isAdd)
+            {
+                answer.push_back(sum);
+            }
+        }
+    }
+    
+    sort(answer.begin(), answer.end());
+    
+    return answer;
+}
