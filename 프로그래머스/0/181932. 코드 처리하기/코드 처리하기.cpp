85f7f62e7c4ff9a4719549adb7bb696eb4e17bba
dog9307 <35515667+dog9307@users.noreply.github.com>
2025-11-05 08:40:51 +0900

    [level 0] Title: 코드 처리하기, Time: 0.43 ms, Memory: 4.02 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string code) {
+    string answer = "";
+    
+    bool mode = false;
+    
+    for (int i = 0; i < code.size(); ++i)
+    {
+        if (code[i] == '1')
+        {
+            mode = !mode;
+            continue;
+        }
+        
+        if (i % 2 == (int)(mode))
+            answer += code[i];
+    }
+    
+    if (answer.size() <= 0)
+        answer = "EMPTY";
+    
+    return answer;
+}
