b839f9f22faa94048caf7ffa4eb195ffa8ed796c
dog9307 <35515667+dog9307@users.noreply.github.com>
2025-11-12 11:01:48 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.03 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+    
+    string odd = "";
+    string even = "";
+    
+    for (auto& num : num_list)
+    {
+        if (num % 2 == 0)
+            even += to_string(num);
+        else
+            odd += to_string(num);
+    }
+    
+    answer = stoi(even) + stoi(odd);
+    
+    return answer;
+}
