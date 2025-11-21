fda7c7d27dade60c70851fba03bc5798eb9a8569
dog9307 <35515667+dog9307@users.noreply.github.com>
2025-11-21 10:15:58 +0900

    [level 0] Title: 배열 만들기 6, Time: 7.36 ms, Memory: 32.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer;
+    
+    for (int i = 0; i < arr.size(); ++i)
+    {
+        if (answer.size() == 0)
+        {
+            answer.push_back(arr[i]);
+        }
+        else 
+        {
+            if (answer.back() == arr[i])
+            {
+                answer.pop_back();
+            }
+            else
+            {
+                answer.push_back(arr[i]);
+            }
+        }
+    }
+    
+    return (answer.size() == 0 ? vector<int>({-1}) : answer);
+}
