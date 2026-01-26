336594f186a8f10d13d71d710f5ab1ba214572d2
dog9307 <35515667+dog9307@users.noreply.github.com>
2026-01-26 20:35:10 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> array, int n) {
+    int answer = 0;
+    
+    return count(array.begin(), array.end(), n);
+}
